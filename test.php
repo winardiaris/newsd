534ab1dd036586571e0f4e55a2677697dac4b7d3
Aris Winardi <me@arwin.my.id>
2015-08-24 15:41:05 +0700

    update

--- test.php
+++ test.php
@@ -4,33 +4,19 @@ include ("static/inc/function.php");
 include ("static/inc/simple_html_dom.php");
 
 
-//foreach(media_list("order by rand()") as $media_list){
-	//$media_id = $media_list[0];
-	//$media_name = $media_list[1];
-	//$media_url = $media_list[2];
+foreach(media_list("order by rand()") as $media_list){
+	$media_id = $media_list[0];
+	$media_name = $media_list[1];
+	$media_url = $media_list[2];
 	
-	//$q = mysql_query("select `url` from `data` where `media`='$media_name' order by `date` DESC limit 50 ") or die(mysql_error());
-	//while($d=mysql_fetch_array($q)){
-		//$url = Balikin($d['url']);
-		//url_get($media_url,$url);
-	//}
-
-//}
-
-$ada =0;
-$url = 'http://borneonews.co.id/berita/10215-uber-rilis-program-beta-android';
-foreach(list_media_prefix('1') as $list){
-	
-	$url_prefix = $list[0];
-	$url = UbahSimbol($url);
-	$pos = strrpos($url,$url_prefix);
-	
-	if($pos ==true){
-		$ada += 1;
+	$q = mysql_query("select `url` from `data` where `media`='$media_name' order by `date` DESC limit 50 ") or die(mysql_error());
+	while($d=mysql_fetch_array($q)){
+		$url = Balikin($d['url']);
+		url_get($media_url,$url);
 	}
-	
+
 }
-echo $ada.PHP_EOL;
+
 
 
 
