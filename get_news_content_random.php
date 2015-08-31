7aced64e861d78cf9351bf1d0dbf1d4b548a3da8
Aris Winardi <me@arwin.my.id>
2015-09-01 02:48:57 +0700

    update

---
+++ get_news_content_random.php
@@ -0,0 +1,26 @@
+<?php
+include ("static/inc/con.php");
+include ("static/inc/function.php");
+include ("static/inc/simple_html_dom.php");
+
+//
+//	#$url_from,
+//	1 = media;
+//	2 = priority
+//	3 = rss
+//	#prefix_status
+//	null = null
+//  1 = active
+//	2 = deleted
+//  #show_get_content
+// 	0=not show content on debug
+// 	1=show content on debug
+//	#save_content
+//	0 = not save
+//	1 = allow save
+//	function get_content($url_from,$prefix_status=null,$show_get_content=null,$save_content=null)
+
+get_content(1,1,0,1);
+get_content(2,1,0,1);
+get_content(3,1,0,1);
+?>
