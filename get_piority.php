d97b2816034f9851ed53d678cbe9f345da89ce24
Aris Winardi <me@arwin.my.id>
2015-08-31 14:54:36 +0700

    update

--- get_piority.php
+++ get_piority.php
@@ -11,7 +11,7 @@ include ("static/inc/simple_html_dom.php");
 foreach (priority_list() as $list){
 	$url = Balikin($list[1]); //url get
 	$media_name = get_media_from_url($url);
-	$media_url = get_media_from_media_data($media_name,'media_url');
+	$media_url = Balikin(get_media_from_media_data($media_name,'media_url'));
 	
 		echo $url.PHP_EOL.PHP_EOL;
 		$html = file_get_html($url);
