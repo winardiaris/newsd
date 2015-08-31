d810d05168a86833bd8d482109322a1fb6668721
Aris Winardi <me@arwin.my.id>
2015-09-01 03:32:24 +0700

    update

--- get_url_from_media.php
+++ get_url_from_media.php
@@ -3,7 +3,7 @@ include ("static/inc/con.php");
 include ("static/inc/function.php");
 include ("static/inc/simple_html_dom.php");
 
-foreach(media_list("order by `media_name` asc") as $media_list){
+foreach(media_list("order by rand()") as $media_list){
 	$media_url = $media_list[2];
 	foreach(url_get($media_url) as $urls){
 		//echo $urls.PHP_EOL;
