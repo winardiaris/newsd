21e9eb376a10a5b6e018a022a49045642f10565f
Aris Winardi <me@arwin.my.id>
2015-10-08 06:40:30 +0700

    update

--- form/footer.php
+++ form/footer.php
@@ -1,28 +1,30 @@
 			</div>
-
 				</div>
             <!-- /.row -->
         </div>
         <!-- /#page-wrapper -->
-
     </div>
     <!-- /#wrapper -->
-
+	<!-- jQuery -->
+	<script src="static/bower_components/jquery/dist/jquery.min.js"></script>
+	<!-- Bootstrap Core JavaScript -->
+	<script src="static/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<!-- Metis Menu Plugin JavaScript -->
+	<script src="static/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+	<!-- Morris Charts JavaScript -->
+	<script src="static/bower_components/raphael/raphael-min.js"></script>
+	<script src="static/bower_components/morrisjs/morris.min.js"></script>
+	<!-- Custom Theme JavaScript -->
+	<script src="static/js/sb-admin-2.js"></script>
+	<script src="static/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+	<script src="static/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+	<script src="static/js/bootstrap-datepicker.js"></script>
 <script >
 $(document).ready(function() {
-     $('#media-table').DataTable({
-                responsive: true
-      });
-     $('#media-table2').DataTable({
-                responsive: true
-      });
-     $('#rss-table').DataTable({
-                responsive: true
-      });
-     $('#priority-table').DataTable({
-                responsive: true
-      });
-      
+		$('#media-table').DataTable({responsive: true});
+		$('#media-table2').DataTable({responsive: true});
+		$('#rss-table').DataTable({responsive: true});
+		$('#priority-table').DataTable({responsive: true});
       
 		$('#UbahXXX').change(function() {
 			locations = $(location).attr('href');
@@ -39,8 +41,5 @@ $(document).ready(function() {
 	});
 });
 </script>
-    
-
 </body>
-
 </html>
--- form/header.php
+++ form/header.php
@@ -1,84 +1,33 @@
 <html lang="en">
-
 <head>
-
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	
 	<title>NEWSD</title>
-	
 	<!-- Bootstrap Core CSS -->
 	<link href="static/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-	
 	<!-- MetisMenu CSS -->
 	<link href="static/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-	
 	<!-- Timeline CSS -->
 	<link href="static/css/timeline.css" rel="stylesheet">
 	<link href="static/css/custom.css" rel="stylesheet">
-	
 	<!-- Custom CSS -->
 	<link href="static/css/sb-admin-2.css" rel="stylesheet">
-	
 	<!-- Morris Charts CSS -->
 	<link href="static/bower_components/morrisjs/morris.css" rel="stylesheet">
-	
 	<!-- Custom Fonts -->
 	<link href="static/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	
 	
 	<link href="static/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
 	<link href="static/bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
 	<link href="static/css/datepicker3.css" rel="stylesheet">
-	
-	
-	
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-	<![endif]-->
-	
-	<!-- jQuery -->
-	<script src="static/bower_components/jquery/dist/jquery.min.js"></script>
-<!--
-	<script src="static/js/jquery.js"></script>
--->
-	
-	<!-- Bootstrap Core JavaScript -->
-	<script src="static/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-	
-	<!-- Metis Menu Plugin JavaScript -->
-	<script src="static/bower_components/metisMenu/dist/metisMenu.min.js"></script>
-	
-	<!-- Morris Charts JavaScript -->
-	<script src="static/bower_components/raphael/raphael-min.js"></script>
-	<script src="static/bower_components/morrisjs/morris.min.js"></script>
-	<!--
-	<script src="static/js/morris-data.js"></script>
-	-->
-	
-	<!-- Custom Theme JavaScript -->
-	<script src="static/js/sb-admin-2.js"></script>
-	
-	<script src="static/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
-	<script src="static/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
-	<script src="static/js/bootstrap-datepicker.js"></script>
-    
-    
-
 </head>
 
 <body>
-
     <div id="wrapper">
 		<?php include("navigation.php");?>
 		<div id="page-wrapper">
             <div class="row">
 					<div class="col-lg-12">
-					
-					
