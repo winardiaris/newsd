1f9bb29750e8944b798f916d6a21f99b14f2ac39
Aris Winardi <me@arwin.my.id>
2015-10-27 11:10:43 +0700

    benerin url-get error

--- static/inc/function/url.php
+++ static/inc/function/url.php
@@ -9,133 +9,6 @@
 		return 0;
 	}
 }
-
-//function url_test($media_url,$url_target){
-	//if(check_internet($url_target)==1){
-		//$html = file_get_html($url_target);
-		//$url_array=array();
-		//foreach($html->find('a') as $data){
-			//$url_get = $data->href;
-			//$valid_url = valid_url($url_get);
-			//$exclude = exclude($url_get);
-			//$check_domain = check_domain_from_db_media($url_get);
-			//$check_prefix_enable = check_prefix_enable($url_get);
-
-			//if($valid_url==1){
-				//if($exclude==0){
-					//if($check_domain>0){
-						//if($check_prefix_enable>0){
-							//array_push($url_array,$url_get);
-						//}
-					//}
-				//}
-			//}
-			//else{
-				//$url_get = real_url($media_url.$url_get);
-				
-				//$valid_url = valid_url($url_get);
-				//$exclude = exclude($url_get);
-				//$check_domain = check_domain_from_db_media($url_get);
-				
-				
-				//if($valid_url==1){
-					//if($exclude==0){
-						//if($check_domain>0){
-							//if($check_prefix_enable>0){
-								//array_push($url_array,$url_get);
-							//}
-						//}
-					//}
-				//}
-				
-				
-			//}
-	
-			////echo $url_get." valid = ".$valid_url." exclude=".$exclude." domain = ".$check_domain."<br>";
-		//}
-		
-		//return $url_array;	
-	//}
-//}
-
-
-//function url_get($media_url,$url_target){	
-	
-	//$array=url_test($media_url,$url_target);
-	//if($array!=0){
-		//$uniq = array_map('unserialize', array_unique(array_map('serialize', $array)));
-		
-		//foreach($uniq as $urls){
-			//$url_id = md5($urls);
-			//if(check_url_from_db($url_id)==0){
-				////printf($urls.PHP_EOL);
-				//url_save($urls);
-			//}
-		//}
-		
-		
-		//foreach(media_list("order by rand()") as $media_list){
-			//$media_url0 = Balikin($media_list[2]);
-			//$array0=url_test($media_url0,$media_url0);
-			//if($array0!=0){
-				//$uniq0 = array_map('unserialize', array_unique(array_map('serialize', $array0)));
-				
-				//foreach($uniq0 as $urls0){
-					//$url_id0 = md5($urls0);
-					//if(check_url_from_db($url_id0)==0){
-						////printf($urls0.PHP_EOL);
-						//url_save($urls1);
-					//}
-					
-				//}
-			//}
-		//}
-		
-		
-		//$qry1 = mysql_query("select * from `url_data` where `url` like '%".UbahSimbol($media_url)."%' and `url_status`='1' limit 20 ")or die(mysql_error());
-		//while($data1=mysql_fetch_array($qry1)){
-			//$array1=url_test($media_url,Balikin($data1['url']));
-			
-			
-			//if($array1!=0){
-				//$uniq1 = array_map('unserialize', array_unique(array_map('serialize', $array1)));
-				
-				//foreach($uniq1 as $urls1){
-					//$url_id1 = md5($urls1);
-					//if(check_url_from_db($url_id1)==0){
-						////printf($urls1.PHP_EOL);
-						//url_save($urls1);
-					//}
-					
-				//}
-			//}
-			
-		//}
-		////------------
-		//$qry2 = mysql_query("select * from `url_data` where `url` like '%".UbahSimbol($media_url)."%' order by rand() limit 20 ")or die(mysql_error());
-		//while($data2=mysql_fetch_array($qry2)){
-			//$array2=url_test($media_url2,Balikin($data2['url']));
-			
-			//if($array2!=0){
-				//$uniq2 = array_map('unserialize', array_unique(array_map('serialize', $array2)));
-				
-				//foreach($uniq2 as $urls2){
-					//$url_id2 = md5($urls2);
-					//if(check_url_from_db($url_id2)==0){
-						////printf($urls2.PHP_EOL);
-						//url_save($urls2);
-					//}
-					
-				//}
-			//}
-		//}
-		
-	//}
-	
-	
-	
-//}
-
 function url_get($url,$url_from){
 	if(check_internet($url)==1){
 	
@@ -146,8 +19,6 @@ function url_get($url,$url_from){
 		$html = file_get_html($url);
 		foreach($html->find('a') as $data){
 			$url_get = $data->href;
-			
-			
 			if(exclude($url_get)==0){
 				
 				if(valid_url($url_get)==1){
@@ -161,7 +32,7 @@ function url_get($url,$url_from){
 					url_save($url_get,$url_from);
 				}
 				elseif(valid_url($url_get)==0){
-					$url_get = real_url($media_url.$url_get);
+					$url_get = real_url($url_get);
 					$url_get = str_replace("//","/",$url_get);
 					$url_get = str_replace(":/","://",$url_get);
 				
---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+include ("static/inc/con.php");
+include ("static/inc/function.php");
+include ("static/inc/simple_html_dom.php");
+
+foreach(media_list("where `media_name` like '%kompas%'") as $media_list){
+	$media_url = $media_list[2];
+	
+	url_get($media_url,1);
+	
+	//foreach(url_get($media_url) as $urls){
+		////echo $urls.PHP_EOL;
+		//url_save($urls,1);		
+	//}
+
+}
+
+?>
